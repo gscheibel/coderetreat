49a0b0af32624dba834068f17b66d01bcaac29ad
Guillaume Scheibel <gscheibel@expedia.com>
2017-10-11 13:32:25 +0100

    Initial commit

---
+++ src/test/java/GameOfLifeTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class GameOfLifeTest {
+
+    @Test
+    public void firstTest() {
+        assertThat(true).isFalse();
+    }
+}
+
+
